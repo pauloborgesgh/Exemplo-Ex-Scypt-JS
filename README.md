3f5a79574f1d1e16600e938b634672e5986c9f9e
pauloborgesgh <79487337+pauloborgesgh@users.noreply.github.com>
2022-09-05 11:57:56 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# js-q82bch
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/js-q82bch)
---
+++ index.js
@@ -0,0 +1,18 @@
+/*let hora = prompt('digite  hora');
+
+if (hora > 6 && hora < 12) {
+  console.log('Bom Dia !');
+} else if (hora > 12 && hora < 18) {
+  console.log('boa Tarde!');
+} else {
+  console.log('Boa Noite!');
+}*/
+
+/* let index = prompt('digite um numero para saber se é par ou impar');
+if (index % 2 !== 0) {
+  index % 2;
+  console.log(index + 'esse numero é impar');
+} else {
+  console.log(index + ' esse numero é par :');
+}
+*/
